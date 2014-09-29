42c4549bfb3702e2aee6cb84eed80af138a14bf4
Angas <angasreid@gmail.com>
2014-09-29 15:17:02 -0400

    Angas Adding Form

--- app.rb
+++ app.rb
@@ -16,3 +16,26 @@ end
 get '/contact' do
   erb :contact
 end
+
+post '/send_mail' do
+
+  m = Mandrill::API.new
+  message = {  
+   :subject=> "Form Submit: Bodacious Bodega",  
+   :from_name=> "Web Automated",  
+   :text=>"Automated",  
+
+   :to=>[
+   	   {  
+      :email=> params[:email],  
+      :name=> params[:name],
+      :comment => params[:comment]
+      }
+   ],  
+   :html=>"<html><h6></h6></html>",  
+   :from_email=>"BodaciousBodega@gmail.com"  
+  }  
+  sending = m.messages.send message  
+  puts "params are" + params.inspect
+
+end
